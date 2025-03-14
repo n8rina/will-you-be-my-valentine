f844888934c6ee209f9b49ac1c8426e886d6acd8
n8rina <nisrinaadiba@students.unnes.ac.id>
2025-03-14 14:58:11 +0700

    making will you be my valentine web based project

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (async function checkForUpdates() {
     const currentVersion = "1.0";
-    const versionUrl = "https://raw.githubusercontent.com/ivysone/Will-you-be-my-Valentine-/main/version.json"; 
+    const versionUrl = "https://raw.githubusercontent.com/n8rina/will-you-be-my-valentine-/main/version.json"; 
 
     try {
         const response = await fetch(versionUrl);
@@ -79,4 +79,8 @@ function handleNoClick() {
 
 function handleYesClick() {
     window.location.href = "yes_page.html";
+}
+
+function handleGoClick() {
+    window.location.href = "";
 }
