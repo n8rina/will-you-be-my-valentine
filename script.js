0eb4e61b76dc94a57af0d6daedc0ec11ee23a7e8
n8rina <nisrinaadiba@students.unnes.ac.id>
2025-03-14 18:13:41 +0700

    update script.js

--- script.js
+++ script.js
@@ -81,6 +81,15 @@ function handleYesClick() {
     window.location.href = "yes_page.html";
 }
 
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
 function handleGoClick() {
-    window.location.href = "";
+    window.location.href = "https://yellowflowerforyourfavs.netlify.app/";
 }
